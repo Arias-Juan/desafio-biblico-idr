f327e99ed6ae2803f24163674cccda4c3cedb1ef
Juan Arias <juan.jose196@hotmail.com>
2022-09-02 10:19:38 -0300

    terminos

--- src/components/Inicio/Terminos.js
+++ src/components/Inicio/Terminos.js
@@ -12,8 +12,9 @@ const Terminos = (props) => {
       <br></br>
       <div className="text-center">
         <p className="lead">El juego trivia biblica se creo con el fin de aprender más de la Biblia y podes interiorizarse más en especial durante este mes de la Biblia.</p>
-        <p className="lead">El juego consta de 4 niveles de dificultad, por cada preguntas con respuesta correcta se suma 100 puntos.</p>
+        <p className="lead">El juego consta de 4 niveles de dificultad, por cada preguntas con respuesta correcta se suman puntos de acuerdo al nivel.</p>
         <p className="lead">¡No es una competencia, pero podes compartir cuantos puntos vas haciendo!</p>
+        <p className="bold">Al finalizar, no te olvides de presionar el boton verde de "Terminar y enviar puntaje", esto envio tu puntaje para luego ver cuanto hiciste.</p>
       </div>
       <div className="text-center">
         <button type="button" onClick={final} className="empezar py-1">
